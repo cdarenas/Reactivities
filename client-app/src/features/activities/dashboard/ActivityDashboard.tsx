22bf0cc4e08c74003063e69978c1c63462ca9632
cdarenas <cdarenas78@hotmail.com>
2020-03-11 13:52:23 -0300

    Add React Form for Activity Creation

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,6 +3,7 @@ import { Grid, GridColumn, List } from 'semantic-ui-react'
 import { IActivity } from '../../../app/models/activity'
 import ActivityList from './ActivityList'
 import ActivityDetails from '../details/ActivityDetails'
+import ActivityForm from '../form/ActivityForm'
 
 interface IProps {
     activities: IActivity[]
@@ -16,6 +17,7 @@ const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
             </Grid.Column>
             <Grid.Column width={6}>
                 <ActivityDetails></ActivityDetails>
+                <ActivityForm></ActivityForm>
             </Grid.Column>
         </Grid>
     )
